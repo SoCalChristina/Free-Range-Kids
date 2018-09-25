e52b9a922112c76b0d0cd4fe33b42f8b743c8d04
Christina <ctovarhamernik@gmail.com>
2018-09-25 02:54:29 -0700

    edit typos

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-
 let allEnemies = [
   new Enemy(-(Math.floor(1 + Math.random() * 5)), 1),
   new Enemy(-(Math.floor(1 + Math.random() * 5)), 2),
   new Enemy(-(Math.floor(1 + Math.random() * 5)), 3)
 ];
+
 // Place the player object in a variable called player
 const player = new Player();
 
@@ -49,19 +49,16 @@ document.addEventListener('keyup', function (e) {
   player.handleInput(allowedKeys[e.keyCode]);
 
 // modal visible when enter key/return key pressed
-  if (e.keyCode === 13 && !gameModal.style.display === hidden) {
-        hideModal();    // Remove the modal function
-    }
+  if (e.keyCode === 13 && !gameModal.style.display === none) {
+    hideModal();    // Remove the modal function
+  }
 });
-
 
 /***************************/
 /*    Game Panel Functions */
 /**************************/
 const panel = document.querySelector('.panel');
 const level = document.querySelector('.level');
-
-
 // Set the heart images on the lives panel
 function setLives() {
   const lives = document.querySelectorAll('.lifebar img');
@@ -76,60 +73,57 @@ function setLives() {
 //TODO create the acheivement awards function
 /*******************************************/
 
+
 /*************************************/
 //            Modal controls        //
 /************************************/
 // source https://www.w3schools.com/howto/howto_css_modals.asp
 const gameModal = document.getElementById('game-modal'); // DOM: Modal
-const closeModal = document.getElementById('closeGame');  // DOM: Close button in modal
 const gameBtn = document.getElementById('game-button');// DOM: Button in modal for either next or again
-//add classes show and hide to toggle modal display
-gameModal.classList.add("show", "hide");
+const closeBtn = document.getElementById('close-game');// DOM: Span close icon
 
-// Moves the modal out of view
+
 const hideModal = () => {
-    if(gameModal.style.display == 'show') { // if modal style class is show modal displays
-      gameModal.style.display = 'none';   // change display style class to none
-  };
+  gameModal.classList.remove('show');
+  gameModal.style.property = "initial"; //initial is display/none
+  gameModal.classList.add("hide");
 }
-// moves the modal into view
-const viewModal = () => {
-      if(gameModal.style.display == 'none') { // if modal class is none modal doesn't display
-          gameModal.style.display = 'show'; //change display style class to show and modal displays
-       };
-     }
 
-//element.classList.toggle("classToRemove", false);
-//Add a class: element.classList.toggle("classToAdd", true);
-
-//closeModal.addEventListener.click(closeModal (hideModal())); // Hide modal on close button click
-//modalBtn.addEventListener.click(hideModal());  // Hide modal on continue button click
-
-//win.onclick = hideModal;
 
  //Dynamically updates the modal message and displays modal
 const showModal = () => {
-    const modalTitle = document.querySelector('.modal-title');      // DOM: Modal title
-    const modalMessage = document.querySelector('.modal-message');  // DOM: Modal message
-    const modalLevel = document.querySelector('.modal-level');      // DOM: Current level value in modal message
+  const modalTitle = document.querySelector('.modal-title');      // DOM: Modal title
+  const modalMessage = document.querySelector('.modal-message');  // DOM: Modal message
+  const modalLevel = document.querySelector('.modal-level');      // DOM: Current level value in modal message
+//check for keep trying or play agan message when player is out of lives
+  if (!player.livesEmpty()) {
+    modalTitle.innerHTML = 'Great Job Free Ranger!';     // Change title
+    modalLevel.innerHTML = String(allEnemies[0].level);   // Change level output
+    modalMessage.innerHTML = `You played outside ${modalLevel.outerHTML} time(s) without your parents!`;   // Change message with level element
+    gameBtn.innerHTML = 'Keep Playing';     // Change button message
 
-    // Not a GameOver
-    if (!player.livesEmpty()) {
-        modalTitle.innerHTML = 'Great Job Free Ranger!';     // Change title
-        modalLevel.innerHTML = String(allEnemies[0].level);   // Change level output
-        modalMessage.innerHTML = `You played outside ${modalLevel.outerHTML} time(s) without your parents!`;   // Change message with level element
-        gameBtn.innerHTML = 'Keep Playing';     // Change button message
-    }
-    // Gameover and same process using diffent input
-    else {
-        modalTitle.innerHTML = 'Game Over';   
-        modalLevel.innerHTML = String(allEnemies[0].level);  
-        modalMessage.innerHTML = `You completed ${modalLevel.outerHTML} level(s).`;  
-        gameBtn.innerHTML = 'Try Again';                                         
-    }
-      viewModal();
-    }
+// modal message input and player life reset
+} else {
+    modalTitle.innerHTML = 'Game Over';
+    modalLevel.innerHTML = String(allEnemies[0].level);
+    modalMessage.innerHTML = `You completed ${modalLevel.outerHTML} level(s).`;
+    gameBtn.innerHTML = 'Try Again';
+  }
+    gameModal.classList.remove('hide');
+    gameModal.classList.add('show');
+};
 
-// Get the modal close button, and when the user clicks on it, execute myFunction
-hideModal()
-gameBtn.onclick = hideModal();
+
+const  displayModal = () => {
+  gameModal.style.property="initial";
+  gameModal.classList.remove('hide');
+  gameModal.classList.add('show');
+};
+
+
+window.onload = function() {
+  hideModal();
+};
+
+//https://www.w3schools.com/jsref/met_element_addeventlistener.asp
+gameBtn.addEventListener("click", hideModal());
