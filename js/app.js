b7e172eedd030db9ac13c5dedfc48fb93ba985e5
Christina <ctovarhamernik@gmail.com>
2018-09-29 17:33:07 -0700

    final edits for first submission

--- js/app.js
+++ js/app.js
@@ -1,129 +1,117 @@
-// starter code provided by udacity
-//tutorial sources https://zoom.us/recording/play/aulotDlzKFegQFIJTaTzKgWvNkVsYtlwO454vL1UPE1Cm6lOUBQCtfVurPOIAGAS?startTime=1529542978000
-//https://www.w3schools.com/howto/howto_css_modals.asp
-
+/* app.js */
 // Now instantiate your objects.
+
+/**************************/
+/* instantiate objects    */
+/*************************/
 // Place all enemy objects in an array called allEnemies
 let allEnemies = [
-  new Enemy(-(Math.floor(1 + Math.random() * 5)), 1),
-  new Enemy(-(Math.floor(1 + Math.random() * 5)), 2),
-  new Enemy(-(Math.floor(1 + Math.random() * 5)), 3)
+    new Enemy(-(Math.floor(1 + Math.random() * 5)), 1),
+    new Enemy(-(Math.floor(1 + Math.random() * 5)), 2),
+    new Enemy(-(Math.floor(1 + Math.random() * 5)), 3)
 ];
 
 // Place the player object in a variable called player
 const player = new Player();
 
-// Pass the list of enemies for changing speed and number
+// access the enemy var to change number rendered and travel speed
 allEnemies.forEach((enemy, index, arr) => {
-  enemy.addObstacles(arr);
+    enemy.addNest(arr);
 });
 
-// List of possible player sprites
-const playerSprites = {
-  boy: 'images/char-boy.png',
-  catGirl: 'images/char-cat-girl.png',
-  hornGirl: 'images/char-horn-girl.png',
-  pinkGirl: 'images/char-pink-girl.png',
-  princess: 'images/char-princess-girl.png'
-};
-
-player.setSpriteBank(playerSprites);
-
-/*******************************/
-// controller key assignments
 /******************************/
-
-//This listens for key presses and sends the keys to your
+/* Key Handler Section      */
+/***************************/
+// This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
 
-  // Allowed keys
-  const allowedKeys = {
-    37: 'left', // Left arrow
-    38: 'up', // Up arrow
-    39: 'right', // Right arrow
-    40: 'down', // Down arrow
-//  13: 'enter'// enter or return
-  };
+    // Allowed keys
+    const allowedKeys = {
+        37: 'left',     // Left arrow
+        38: 'up',       // Up arrow
+        39: 'right',    // Right arrow
+        40: 'down',     // Down arrow
+    };
 
-  player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]); // Passed an allowed key value to control player movements
 
-// modal visible when enter key/return key pressed
-  if (e.keyCode === 13 && !gameModal.style.display === none) {
-    hideModal();    // Remove the modal function
-  }
+    // The 'Enter' key is pressed and the modal is visible
+    if (e.keyCode === 13 && !modal.classList.contains('out')) {
+        hideModal();    // Remove the modal
+    }
 });
 
-/***************************/
-/*    Game Panel Functions */
-/**************************/
-const panel = document.querySelector('.panel');
-const level = document.querySelector('.level');
-// Set the heart images on the lives panel
+/********************************/
+/* Player Game life Display    */
+/*****************************/
+const panel = document.querySelector('.panel');     // DOM: Game data panel
+const level = document.querySelector('.level');     // DOM: Level indicator
+// code source for canvas https://www.w3schools.com/tags/ref_canvas.asp
+panel.style.margin = "auto";
+
+// Display player life hearts on score board panel
 function setLives() {
-  const lives = document.querySelectorAll('.lifebar img');
-//lives will be set to 3
-  lives.forEach((life) => {
-//player will have 3 hearts on score panel at start of game
-    life.src = `./${heart.sprite}`;
-  });
+    const lives = document.querySelectorAll('.lifebar img');
+
+    lives.forEach((life) => {
+        life.src = `./${player.sprite}`;
+    });
 }
+/* TODO create acheivement section
+function displayAchievements() {
+    const awards = document.querySelectorAll('.achievement-vault img');
 
-/********************************************/
-//TODO create the acheivement awards function
-/*******************************************/
+    awards.forEach((achivement) => {
+        achievement.src = `./${award.sprite}`;//./before ${achievement.sprite}
+    });
+}
+*/
 
+/*********************/
+/* Footer section   */
+/*********************/
+const footer = document.querySelector('.footer');
+// code source for canvas https://www.w3schools.com/tags/ref_canvas.asp
+footer.style.position = "absolute";
+footer.style.top = "750px";
+footer.style.margin = "auto";
 
-/*************************************/
-//            Modal controls        //
-/************************************/
-// source https://www.w3schools.com/howto/howto_css_modals.asp
-const gameModal = document.getElementById('game-modal'); // DOM: Modal
-const gameBtn = document.getElementById('game-button');// DOM: Button in modal for either next or again
-const closeBtn = document.getElementById('close-game');// DOM: Span close icon
+/*********************/
+/*  Modal Section   */
+/*******************/
+// source: https://www.w3schools.com/bootstrap/bootstrap_modal.asp
+const modal = document.querySelector('.modal'); // DOM: Modal
+const modalCloseBtn = document.querySelector('.modal-btn-close'); // DOM: Close button in modal
+const modalContBtn = document.querySelector('.modal-btn-cont'); // DOM: Button in modal for either next or again
 
-
+/** Make Model disapear **/
 const hideModal = () => {
-  gameModal.classList.remove('show');
-  gameModal.style.property = "initial"; //initial is display/none
-  gameModal.classList.add("hide");
-}
+    modal.classList.add('out');
+};
 
-
- //Dynamically updates the modal message and displays modal
+/** Check Modal Data Input **/
 const showModal = () => {
-  const modalTitle = document.querySelector('.modal-title');      // DOM: Modal title
-  const modalMessage = document.querySelector('.modal-message');  // DOM: Modal message
-  const modalLevel = document.querySelector('.modal-level');      // DOM: Current level value in modal message
-//check for keep trying or play agan message when player is out of lives
-  if (!player.livesEmpty()) {
-    modalTitle.innerHTML = 'Great Job Free Ranger!';     // Change title
-    modalLevel.innerHTML = String(allEnemies[0].level);   // Change level output
-    modalMessage.innerHTML = `You played outside ${modalLevel.outerHTML} time(s) without your parents!`;   // Change message with level element
-    gameBtn.innerHTML = 'Keep Playing';     // Change button message
+    const modalTitle = document.querySelector('.modal-title'); // DOM: Modal title
+    const modalMessage = document.querySelector('.modal-message'); // DOM: Modal message
+    const modalLevel = document.querySelector('.modal-level'); // DOM: Current level value in modal message
 
-// modal message input and player life reset
-} else {
-    modalTitle.innerHTML = 'Game Over';
-    modalLevel.innerHTML = String(allEnemies[0].level);
-    modalMessage.innerHTML = `You completed ${modalLevel.outerHTML} level(s).`;
-    gameBtn.innerHTML = 'Try Again';
-  }
-    gameModal.classList.remove('hide');
-    gameModal.classList.add('show');
+    // Player completed level - game continues
+    if (!player.livesEmpty()) {
+        modalTitle.innerHTML = 'Range On!'; // Change title
+        modalLevel.innerHTML = String(allEnemies[0].level); // Change level
+        modalMessage.innerHTML = `You survived ${modalLevel.outerHTML} walk(s) alone.`; // Change message with level element
+        modalContBtn.innerHTML = 'Keep Exploring'; // Change button
+    }
+    // Gameover
+    else {
+        modalTitle.innerHTML = 'Game Over'; // Change title
+        modalLevel.innerHTML = String(allEnemies[0].level); // Change level
+        modalMessage.innerHTML = `You survived ${modalLevel.outerHTML} free range expeditions.`; // Change message with level element
+        modalContBtn.innerHTML = 'Play agian'; // Change button
+    }
+    modal.classList.remove('out'); // Put modal into view
 };
 
-
-const  displayModal = () => {
-  gameModal.style.property="initial";
-  gameModal.classList.remove('hide');
-  gameModal.classList.add('show');
-};
-
-
-window.onload = function() {
-  hideModal();
-};
-
-//https://www.w3schools.com/jsref/met_element_addeventlistener.asp
-gameBtn.addEventListener("click", hideModal());
+modalCloseBtn.addEventListener('click', hideModal); // Hide modal on close button click
+modalContBtn.addEventListener('click', hideModal); // Hide modal on continue button click
